8316f69efd9f48a2babe7d284bb2a6cff9f6f2bf
Léo Guillaume <leo.guillaume@epitech.eu>
2024-01-14 11:39:36 +0100

    feat: add sitemap

--- nuxt.config.js
+++ nuxt.config.js
@@ -46,7 +46,34 @@ export default {
 
   // Modules: https://go.nuxtjs.dev/config-modules
   modules: [
+    '@nuxtjs/sitemap'
   ],
+
+  sitemap: {
+    hostname: website.url,
+    routes: [
+      { url: '/', changefreq: 'monthly', priority: 1.0, lastmod: new Date().toISOString() },
+      { url: '/juridique', changefreq: 'monthly', priority: 0.7, lastmod: new Date().toISOString() },
+      { url: '/reglement', changefreq: 'weekly', priority: 0.8, lastmod: new Date().toISOString() },
+      { url: '/reglement/general', changefreq: 'weekly', priority: 0.7, lastmod: new Date().toISOString() },
+      { url: '/reglement/general/lexique', changefreq: 'monthly', priority: 0.6, lastmod: new Date().toISOString() },
+      { url: '/reglement/general/zones', changefreq: 'monthly', priority: 0.6, lastmod: new Date().toISOString() },
+      { url: '/reglement/general/interdictions', changefreq: 'monthly', priority: 0.6, lastmod: new Date().toISOString() },
+      { url: '/reglement/general/mort-rp', changefreq: 'monthly', priority: 0.6, lastmod: new Date().toISOString() },
+      { url: '/reglement/general/coma', changefreq: 'monthly', priority: 0.6, lastmod: new Date().toISOString() },
+      { url: '/reglement/illegal', changefreq: 'weekly', priority: 0.7, lastmod: new Date().toISOString() },
+      { url: '/reglement/illegal/gang', changefreq: 'weekly', priority: 0.6, lastmod: new Date().toISOString() },
+      { url: '/reglement/illegal/criminel', changefreq: 'weekly', priority: 0.6, lastmod: new Date().toISOString() },
+      { url: '/reglement/illegal/organisation', changefreq: 'weekly', priority: 0.6, lastmod: new Date().toISOString() },
+      { url: '/reglement/legal', changefreq: 'weekly', priority: 0.7, lastmod: new Date().toISOString() },
+      { url: '/reglement/legal/civil', changefreq: 'monthly', priority: 0.6, lastmod: new Date().toISOString() },
+      { url: '/reglement/legal/lspd-bcso', changefreq: 'monthly', priority: 0.6, lastmod: new Date().toISOString() },
+      { url: '/reglement/legal/ems', changefreq: 'monthly', priority: 0.6, lastmod: new Date().toISOString() },
+      { url: '/reglement/legal/entreprises', changefreq: 'monthly', priority: 0.6, lastmod: new Date().toISOString() },
+      { url: '/touches', changefreq: 'yearly', priority: 0.5, lastmod: new Date().toISOString() }
+    ]
+  },
+
 
   features: {
     store: false,
